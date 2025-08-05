e1be461a046d769d43f0bee4ad930f49cd5cd06a
Lamlam26072004 <lamlqph33093@fpt.edu.vn>
2025-08-05 22:45:35 +0700

    category them va hien thi

---
+++ mvc-oop-basic-duanmau/mvc-oop-basic-duanmau/classes/category.php
@@ -0,0 +1,52 @@
+<?php
+include '../lib/database.php';
+include '../helpers/format.php';
+?>
+<?php
+    class category 
+     {
+        private $db;
+        private $fm;
+        
+        public function __construct()  
+        {
+            $this->db = new Database();
+            $this->fm = new Format();
+
+        }
+        public function insert_category($catName) 
+        {
+            $catName = $this->fm->validation($catName);
+            
+            $catName = mysqli_real_escape_string($this->db->link, $catName);
+
+            if (empty($catName)) {
+                $alert = "<span class='error'>Category Name must not be empty!</span>";
+                return $alert;
+            }else {
+                $query = "INSERT INTO tbl_category(catName) VALUES('$catName')";
+                $result = $this->db->insert($query);
+                if ($result) {
+                    $alert = "<span class='success'>Category Inserted Successfully!</span>";
+                    return $alert;
+                }else{
+                    $alert = "<span class='error'>Category Not Inserted!</span>";
+                    return $alert;
+                }
+            }
+        }
+        public function show_category()
+        {
+            $query = "SELECT * FROM tbl_category ORDER BY catId DESC";
+            $result = $this->db->select($query);
+            return $result;
+        }
+        public function getCategoryById($id)
+        {
+            $query = "SELECT * FROM tbl_category WHERE catId = '$id'";
+            $result = $this->db->select($query);
+            return $result;
+        }
+     }
+     
+?>
